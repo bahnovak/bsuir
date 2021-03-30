d556e456e6574f9ffaf895a6c3735bb9a0bbbdef
Roma <zohanblz@gmail.com>
2021-03-30 12:53:21 +0300

    add task

---
+++ discrete-mathematics_combinatorics.js
@@ -0,0 +1,37 @@
+const elements = ['aa' ,'b', 'cc', 'ddd'];
+
+const factorial = (n) => {
+    return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+
+const getCombinations = (lng = 2) => {
+    if (lng <= 0 || !Number.isInteger(lng) || typeof lng !== 'number') {
+        throw new Error('Incorrect data. Expected: 1,2,3...')
+    }
+
+    const countCombinatins = factorial(elements.length + lng - 1) / (factorial(lng) * factorial(elements.length - 1));
+
+    const data = new Array(lng);             
+    const results = [];
+
+    function recurs(pos = 0, start = 0) {        
+      if(pos === lng) {                
+        results.push(data.slice());  
+        return;
+      }
+
+      for(let i = start; i < elements.length; i += 1) {
+        data[pos] = elements[i];         
+        recurs(pos + 1, i);                 
+      }
+    };
+    recurs();
+
+    console.log(`Количество сочетаний: ${countCombinatins}`);
+    console.log(results); 
+
+    return results;  
+}
+
+getCombinations(3);
